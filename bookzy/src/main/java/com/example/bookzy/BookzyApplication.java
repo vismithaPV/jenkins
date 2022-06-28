cdafd2e0ea1434a6f7147cc7c682f6973a30db64
Vismitha <vismi1995@gmail.com>
2022-06-28 09:16:32 +0530

    Springboot bookzy app added

---
+++ bookzy/src/main/java/com/example/bookzy/BookzyApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookzyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookzyApplication.class, args);
+	}
+
+}
